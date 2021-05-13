6d9eacf073353b08721f77e1d02a24967a054acd
Learner045 <shreya.ds045@gmail.com>
2021-05-12 21:05:01 -0400

    registration

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -10,7 +10,7 @@ function MenuBar(){
     const path = pathname === '/' ? 'home' : pathname.substr(1);
     const [activeItem, setActiveItem] = useState(path);
 
-   const handleItemClick = (e, { name }) => setActiveItem(name)
+    const handleItemClick = (e, { name }) => setActiveItem(name)
 
     return (
 
